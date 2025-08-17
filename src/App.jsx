39d79052d5764d67b26fff1e52eed7713380e6d7
fiddleswe <smartsoodprasert69@gmail.com>
2025-08-18 01:55:27 +0700

    Introducing browser-router from React-Router into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Home from '../src/pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Home/>   
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
