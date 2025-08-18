a48df5bc32a90d333de5a0314de5506bffb9dd75
fiddleswe <smartsoodprasert69@gmail.com>
2025-08-18 12:22:06 +0700

    created data folder to store data for each pages, reducing messy or large codebase.

---
+++ src/data/projects.jsx
@@ -0,0 +1,31 @@
+function projectData() {    
+    return [
+        {
+            title: "Project One",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            tools: ["React", "Vite", "TailwindCSS", "Shadcn"],
+            liveLink: "#",
+            preview: "#",
+            repoUrl: "#",
+        },
+        {
+            title: "Project Two",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            tools: ["React", "Vite", "TailwindCSS", "Shadcn"],
+            liveLink: "#",
+            preview: "#",
+            repoUrl: "#",
+        },
+        {
+            title: "Project Three",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            tools: ["React", "Vite", "TailwindCSS", "Shadcn"],
+            liveLink: "#",
+            preview: "#",
+            repoUrl: "#",
+        },
+    ]
+}
+
+
+export default projectData
