be8e7c702ff691d8970f48116cb45adc1e6e9ae4
fiddleswe <smartsoodprasert69@gmail.com>
2025-08-18 17:44:59 +0700

    refactoring seperation of concerns on about page content

--- src/data/about.jsx
+++ src/data/about.jsx
@@ -1,21 +1,73 @@
-function aboutData(){
-
-    return [
+function aboutData() {
+  return [
+    {
+      title: "About",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      title: "Skills",
+      categories: [
         {
-            title: "About",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          subtitle: "Frontend",
+          skills: [
+            { name: "HTML", icon: "" },
+            { name: "CSS", icon: "" },
+            { name: "Javascript", icon: "" },
+            { name: "React", icon: "" },
+            { name: "TailwindCSS", icon: "" },
+            { name: "Vite", icon: "" },
+            { name: "Shadcn", icon: "" },
+            { name: "Boostrap", icon: "" },
+          ],
         },
         {
-            title: "Skills",
-            subtitle: "Frontend",
-            technology: "",
-            subtitle: "Backend",
-            technology: "",
+          subtitle: "Backend",
+          skills: [
+            { name: "Nodejs", icon: "" },
+            { name: "MongoDB", icon: "" },
+            { name: "Docker", icon: "" },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Certification",
+      categories: [
+        {
+          subtitle: "Competitions",
+          certificates: [
+            { name: "Competition-1", year: "", imageUrl: "" },
+            { name: "Competition-2", year: "", imageUrl: "" },
+          ],
         },
         {
-            title: "Certification",
-        }
-    ]
+          subtitle: "Programming Bootcamp",
+          certificates: [
+            { name: "Certificate-1", year: "", imageUrl: "" },
+            { name: "Certificate-2", year: "", imageUrl: "" },
+            { name: "Certificate-3", year: "", imageUrl: "" },
+          ],
+        },
+        {
+          subtitle: "Online Course",
+          certificates: [
+            { name: "Certificate-1", year: "", imageUrl: "" },
+            { name: "Certificate-2", year: "", imageUrl: "" },
+            { name: "Certificate-3", year: "", imageUrl: "" },
+          ],
+        },
+        {
+          subtitle: "Camp",
+          certificates: [
+            { name: "Certificate-1", year: "", imageUrl: "" },
+            { name: "Certificate-2", year: "", imageUrl: "" },
+            { name: "Certificate-3", year: "", imageUrl: "" },
+          ],
+        },
+      ],
+    },
+  ];
 }
 
 export default aboutData;
